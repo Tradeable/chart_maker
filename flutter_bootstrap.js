644b4cdfd50b7e62c780b212d9e2fe5ac2f4d598
adityagohad <adityagohad@gmail.com>
2025-12-09 15:42:44 +0530

    dashboard ready

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1845744416"
+    serviceWorkerVersion: "3207510145"
   }
 });
